6693dd5e47090e2c6c2a8b3d10edf74b4b68fefa
elsa <elsa@gmail.com>
2024-11-04 14:13:15 +0100

    Mensajes

---
+++ src/main/java/es/santander/ascender/Saludo.java
@@ -0,0 +1,9 @@
+package es.santander.ascender;
+import java land String
+
+
+public class Saludo {
+    public String saludar(String nombre) {
+     return "Hola " + nombre;
+    }
+}
---
+++ src/test/java/es/santander/ascender/SaludoTest.java
@@ -0,0 +1,16 @@
+package es.santander.ascender;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SaludoTest {
+    @Test 
+    public void testSaludar () {
+      Saludo saludo = new Saludo ();
+      String resultado = saludo.saludar ("Fernando");
+      assertEquals(saludo "Hola Fernando",resultado);
+
+    }
+}
+ 
